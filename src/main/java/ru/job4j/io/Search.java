4daab2848af5d03c29bccac5aed6f1fa45daa809
Иван <ivan1990kalinin@yandex.ru>
2024-12-20 22:23:26 +0700

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,8 +11,9 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
@@ -19,5 +21,15 @@ public class Search {
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
 
+    }
+
+    public static void validate(String[] args) {
+        File root = new File(args[0]);
+       if (args.length < 2 || !root.exists() || !root.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Корневая папка %s не существует", root.getAbsolutePath()));
+        }
+        if (!args[1].endsWith(".js")) {
+            throw new IllegalArgumentException("Файл не найден");
+        }
     }
 }
