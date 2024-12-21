2491ed2988fb8bb793a1e3b41c0d0c7e84f7a502
Иван <ivan1990kalinin@yandex.ru>
2024-12-21 11:25:49 +0700

    12. Валидация параметров запуска [#246865 #526048]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -24,9 +24,15 @@ public class Search {
     }
 
     public static void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IndexOutOfBoundsException("Не задан параметр начальной папки");
+        }
         File root = new File(args[0]);
-       if (args.length < 2 || !root.exists() || !root.isDirectory()) {
+        if (!root.exists() || !root.isDirectory()) {
             throw new IllegalArgumentException(String.format("Корневая папка %s не существует", root.getAbsolutePath()));
+        }
+        if (args.length < 2) {
+            throw new IndexOutOfBoundsException("Не задан параметр расширения файла");
         }
         if (!args[1].endsWith(".js")) {
             throw new IllegalArgumentException("Файл не найден");
