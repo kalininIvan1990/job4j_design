6621aae825f4581309f5e42cd237b96bbf8eacff
Иван <ivan1990kalinin@yandex.ru>
2025-03-04 23:19:22 +0700

    2. Что такое Socket? [#4850 #534631]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,11 +1,13 @@
 package ru.job4j.io;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -15,10 +17,17 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String[] param = in.readLine().split(" ");
-                    if (param[1].endsWith("Bye")) {
-                        server.close();
-                        System.out.println("Сервер прекратил работу");
+                    String requestLine = in.readLine();
+                    String[] parts = requestLine.split("[?= ]");
+                    if (parts.length > 3 && "msg".equals(parts[2])) {
+                        out.write(parts[3].getBytes());
+                        if ("bye".equalsIgnoreCase(parts[3])) {
+                            out.write("\nthe server has stopped".getBytes());
+                            server.close();
+                            System.out.println("сервер прекращает работу");
+                        }
+                    } else {
+                        out.write("Invalid key".getBytes());
                     }
                     out.flush();
                 }
